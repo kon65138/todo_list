9a4bad4d5c0b94de533adb3b0ddf14fa5706ef4c
learning <156806318+kon65138@users.noreply.github.com>
2024-09-11 14:58:33 +0100

    fix todo css + renderTodo

--- src/modules/allTasks.js
+++ src/modules/allTasks.js
@@ -14,4 +14,12 @@ export function loadAllTasks () {
     const test = new todo ("Miles", "is not cool", "11/12/2005", "high", false);
     test.renderTodo();
 
+    const test1 = new todo ("luke", "test description to see how it copes with long stuff lmao lets gooo vaveiavhaeovaovahvoivhoaivheovihvghagoig", "11/09/2001", "medium", true);
+    test1.renderTodo();
+
+    const test2 = new todo ("test3", "test description is noraml length ig", "date", "low", false);
+    test2.renderTodo()
+
+
+
 };
--- src/modules/todo.js
+++ src/modules/todo.js
@@ -13,19 +13,19 @@ export class todo {
     renderTodo(){
         const domElements = [
             {element: 'div', class: 'task', id: `${this.name}`, parent: '.tasksContainer'},
-            {element: 'div', class: 'taskLeft', parent: '.task'},
-            {element: 'input', type: 'checkbox', parent: '.taskLeft'},
-            {element: 'div', class: 'taskMiddle', parent: '.task'},
-            {element: 'div', class: 'taskTitle', textContent: `${this.name}`, parent: '.taskMiddle'},
-            {element: 'div', class: 'taskDescription', textContent: `${this.description}`, parent: '.taskMiddle'},
-            {element: 'div', class: 'taskRight', parent: '.task'},
-            {element: 'div', class: 'taskPriority', textContent: `${this.priority}`, parent: '.taskRight'},
-            {element: 'div', class: 'taskDate', textContent: `${this.dueDate}`, parent: '.taskRight'},
-            {element: 'div', class: 'DltEdt', parent: '.task'},
-            {element: 'button', class: 'taskEdit', parent: '.DltEdt'},
-            {element: 'img', id: 'editIcon', parent: '.taskEdit'},
-            {element: 'button', class: 'taskDelete', parent: '.DltEdt'},
-            {element: 'img', id: 'deleteIcon', parent: '.taskDelete'},
+            {element: 'div', class: 'taskLeft', parent: `#${this.name}`},
+            {element: 'input', type: 'checkbox', parent: `#${this.name} .taskLeft`},
+            {element: 'div', class: 'taskMiddle', parent: `#${this.name}`},
+            {element: 'div', class: 'taskTitle', textContent: `${this.name}`, parent: `#${this.name} .taskMiddle`},
+            {element: 'div', class: 'taskDescription', textContent: `${this.description}`, parent: `#${this.name} .taskMiddle`},
+            {element: 'div', class: 'taskRight', parent: `#${this.name}`},
+            {element: 'div', class: 'taskPriority', textContent: `${this.priority}`, parent: `#${this.name} .taskRight`},
+            {element: 'div', class: 'taskDate', textContent: `${this.dueDate}`, parent: `#${this.name} .taskRight`},
+            {element: 'div', class: 'DltEdt', parent: `#${this.name}`},
+            {element: 'button', class: 'taskEdit', parent: `#${this.name} .DltEdt`},
+            {element: 'img', id: 'editIcon', parent: `#${this.name} .taskEdit`},
+            {element: 'button', class: 'taskDelete', parent: `#${this.name} .DltEdt`},
+            {element: 'img', id: 'deleteIcon', parent: `#${this.name} .taskDelete`},
         ];
 
         render(domElements);
